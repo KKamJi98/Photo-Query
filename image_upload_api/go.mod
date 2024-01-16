d408fd8ae0a7f5ed4a11ec2c45fab93037584f3f
KKam._.Ji <xowl5460@naver.com>
2024-01-17 01:27:12 +0900

    UPDATE post update api

--- image_upload_api/go.mod
+++ image_upload_api/go.mod
@@ -1,12 +1,13 @@
 module github.com/War-Oxi/ACS-Team-KKamJi
 
-go 1.23
+go 1.20
 
 require (
 	github.com/aws/aws-sdk-go v1.49.21
 	github.com/gin-gonic/gin v1.9.1
 	github.com/go-sql-driver/mysql v1.7.1
 	github.com/google/uuid v1.5.0
+	github.com/joho/godotenv v1.5.1
 )
 
 require (
--- main.go
+++ main.go
@@ -1,28 +1,48 @@
 package main
 
 import (
-	"net/http"
-	"github.com/gin-gonic/gin"
 	"database/sql"
-	// "fmt"
+	"log"
+	// "net/http"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"os"
+
+	"fmt"
 	// "log"
 	_ "github.com/go-sql-driver/mysql"
 )
 
+type Post struct {
+	PostID int64 `json:"post_id"`
+}
 
 func main() {
-	db, err := sql.Open("mysql", "root:1234@tcp(localhost:3306)/world")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbEndpoint := os.Getenv("DB_ENDPOINT")
+	dbName := os.Getenv("DB_NAME")
+
+	db, err := sql.Open("mysql", fmt.Sprintf("%v:%v@tcp(%v:3306)/%v", dbUser, dbPassword, dbEndpoint, dbName))
+
+	if db == nil {
+		fmt.Println("Can't connect database")
+	}
 	router := gin.Default()
-	
-	router.POST("/api-v1/posts") // 포스트 생성
-	router.GET("/api-v1/posts") // 포스트 조회
-	router.PATCH("/api-v1/posts") // 포스트 수정
-	router.DELETE("/api-v1/posts") // 포스트 삭제
+
+	router.POST("/api-v1/posts")                // 포스트 생성
+	router.GET("/api-v1/posts")                 // 포스트 조회
+	router.PATCH("/api-v1/posts")               // 포스트 수정
+	router.DELETE("/api-v1/posts")              // 포스트 삭제
 	router.POST("/api-v1/posts/{postId}/likes") // 좋아요 기능
-	router.GET("/api-v1/posts/{postId}/likes") // 좋아요 조회 (Post 기준)
-	router.GET("/api-v1/users/{postId}/likes") // 좋아요 조회 (User 기준)
-	router.GET("/api-v1/posts/{postId}/rank") // 포스트 랭킹 조회
+	router.GET("/api-v1/posts/{postId}/likes")  // 좋아요 조회 (Post 기준)
+	router.GET("/api-v1/users/{postId}/likes")  // 좋아요 조회 (User 기준)
+	router.GET("/api-v1/posts/{postId}/rank")   // 포스트 랭킹 조회
 
 	router.Run(":8080")
 }
