9c979351026cc16587f3bebcc518cc6abad16deb
kkankkandev <xowl5460@naver.com>
2024-01-31 19:31:31 +0900

    file context timeout upload

--- pictures/Create.go
+++ pictures/Create.go
@@ -184,7 +184,7 @@ func uploadToS3(fileReader io.Reader, fileName string, sess *session.Session, er
 	uuid := uuid.New()
 	// log.Printf("uuid=> %v", uuid.String())
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
 	defer cancel()
 	fileExtension := getFileExtension(fileName)
 
