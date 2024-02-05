647ccd855664fc510207849acf41a3c7dc06dae7
kkankkandev <xowl5460@naver.com>
2024-02-05 12:19:44 +0900

    custom Marshal, Unmarshal

--- pictures/Model.go
+++ pictures/Model.go
@@ -21,27 +21,27 @@ type CustomTime struct {
 }
 
 // MarshalJSON is a custom marshaller that omits the Valid field and formats the Time.
-func (ct *CustomTime) MarshalJSON() ([]byte, error) {
-	if !ct.Valid {
-		return []byte("null"), nil
-	}
-	// You can also format the time as you wish here.
-	return json.Marshal(ct.Time.Format(time.RFC3339))
-}
+// func (ct *CustomTime) MarshalJSON() ([]byte, error) {
+// 	if !ct.Valid {
+// 		return []byte("null"), nil
+// 	}
+// 	// You can also format the time as you wish here.
+// 	return json.Marshal(ct.Time.Format(time.RFC3339))
+// }
 
-// UnmarshalJSON is a custom unmarshaller that parses time and sets Valid field accordingly.
-func (ct *CustomTime) UnmarshalJSON(data []byte) error {
-	// If the data is "null", set Valid to false.
-	if string(data) == "null" {
-		ct.Valid = false
-		return nil
-	}
-	// Assume the input is in RFC3339 format for parsing.
-	t, err := time.Parse(`"`+time.RFC3339+`"`, string(data))
-	if err != nil {
-		return err
-	}
-	ct.Valid = true
-	ct.Time = t
-	return nil
-}
+// // UnmarshalJSON is a custom unmarshaller that parses time and sets Valid field accordingly.
+// func (ct *CustomTime) UnmarshalJSON(data []byte) error {
+// 	// If the data is "null", set Valid to false.
+// 	if string(data) == "null" {
+// 		ct.Valid = false
+// 		return nil
+// 	}
+// 	// Assume the input is in RFC3339 format for parsing.
+// 	t, err := time.Parse(`"`+time.RFC3339+`"`, string(data))
+// 	if err != nil {
+// 		return err
+// 	}
+// 	ct.Valid = true
+// 	ct.Time = t
+// 	return nil
+// }
