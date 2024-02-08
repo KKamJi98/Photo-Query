bd8cb9263432583fd270c4703fb511f8d00f3297
kkankkandev <xowl5460@naver.com>
2024-02-08 14:22:34 +0900

    fix: GetMethod lastindex

--- pictures/Get.go
+++ pictures/Get.go
@@ -5,9 +5,9 @@ import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	"log"
+	"math"
 	"net/http"
 	"strconv"
-	"math"
 )
 
 // GetPictures 함수는 데이터베이스에서 모든 사진을 가져옵니다.
@@ -55,21 +55,8 @@ func GetPicturesByUserId(c *gin.Context) {
 
 	last := c.Query("last")
 	if last == "" {
-		var pictureID string
-		// 단일 행 조회에는 QueryRow를 사용, Scan 메소드로 결과 추출
-		err := db.QueryRow("SELECT picture_id FROM Pictures WHERE user_id = ? ORDER BY picture_id DESC LIMIT 1", userId).Scan(&pictureID)
-		if err != nil {
-			log.Printf("Error retrieving last picture: %v", err)
-			// return // 에러 시 함수 종료 혹은 적절한 에러 처리
-			pictureID = strconv.Itoa(math.MaxInt64 - 1)
-		}
-		lastIndex, err := strconv.Atoi(pictureID)
-		if err != nil {
-			log.Printf("Error converting pictureID to int: %v", err)
-			return // 변환 에러 시 함수 종료 혹은 적절한 에러 처리
-		}
-		lastIndex += 1                 // lastIndex 값을 1 증가
-		last = strconv.Itoa(lastIndex) // 다시 문자열로 변환
+		last = strconv.Itoa(math.MaxInt64)
+		log.Printf("%s", "last index not found")
 	}
 	log.Printf("last: %s", last)
 
